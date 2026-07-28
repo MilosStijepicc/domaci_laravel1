e76faf967978f895394c9df8d8a9e0416800cc33
MilosStijepicc <stijepicmiloss@gmail.com>
2026-07-28 13:19:12 +0200

    API u web shop-u

---
+++ storage/framework/views/c3d21c97608dcc8adb708e78b9c2549f.php
@@ -0,0 +1,5 @@
+<form method="POST" action="<?php echo e(route('logout')); ?>">
+    <?php echo csrf_field(); ?>
+    <button type="submit">Logout</button>
+</form>
+<?php /**PATH C:\Users\Korisnik\Desktop\domaci\resources\views/logout.blade.php ENDPATH**/ ?>
